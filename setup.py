b5e4d31f7c615dadbc383026b6ddce5862c18063
bbrodriges <bender.rodriges@gmail.com>
2016-02-03 15:25:54 +0300

    setup script

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='luthor',
+      version='1.0',
+      description='A simple library to parse XML',
+      author='bbrodriges',
+      author_email='bender.rodriges@gmail.com',
+      url='https://github.com/bbrodriges/luthor',
+      requires=['lxml']
+    )
